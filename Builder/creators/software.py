1e2634a6d1e3720dab99079088b631f2fa1d0db5
m4g0med0v <157313447+m4g0med0v@users.noreply.github.com>
2025-03-10 17:14:43 +0300

    Update software.py

--- Builder/creators/software.py
+++ Builder/creators/software.py
@@ -27,13 +27,13 @@ class PythonBuilder:
         python_version = input()
         cpu_cores = int(os.cpu_count() * 0.5)
 
-        cd_download = "cd ~/Downloads"
+        cd_download = "cd /tmp"
         curl = f'curl -L -o "Python-{python_version}.tar.xz" "https://www.python.org/ftp/python/{python_version}/Python-{python_version}.tar.xz"'
         configure = f"./configure --prefix=$HOME/.python/python{python_version} --enable-optimizations"
         fish = rf'echo "set -x PATH \$PATH \$HOME/.python/python{python_version}/bin" >> ~/.config/fish/config.fish'
 
         os.system("mkdir ~/.python/")
-        os.system(f'{cd_download} && {curl}"')
+        os.system(f'{cd_download} && {curl}')
         os.system(f'{cd_download} && tar -xvf Python-{python_version}.tar.xz')
         os.system(f'{cd_download}/Python-{python_version} && {configure}')
         os.system(f'{cd_download}/Python-{python_version} && make -j{cpu_cores}')
